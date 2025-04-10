0705b0896dda289b634f0ff55ec3491eb5cc8852
Parth Parmar <parth.parmar@fxis.ai>
2025-04-10 14:50:27 +0530

    Initial commit

---
+++ playground/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# import HttpRsponse
+
+# request => response 
+
+def say_hello(request):
+    # pull data from db
+    # tranform data
+    # send email
+    return HttpResponse("hello world")
+
